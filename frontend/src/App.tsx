999fef00364962110888149fef75c3dd19fbbb27
Eline Evje <elineev@stud.ntnu.no>
2023-07-27 14:09:56 +0200

    Started creating login function in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import LoginPage from "./view/LoginPage";
+import Login from "./pages/Login";
+import Nav from "./components/Nav";
+import { BrowserRouter, Route, Router } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
 
 function App() {
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("http://localhost:8000/api/user", {
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+      });
+
+      const content = await response.json();
+
+      setName(content.name);
+    })();
+  });
+
   return (
-    <div className="App">
-      <LoginPage />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Nav name={name} setName={setName} />
+
+        <main className="form-signin">
+          <Route path="/" exact component={() => <Home name={name} />} />
+          <Route path="/login" component={() => <Login setName={setName} />} />
+          <Route path="/register" component={Register} />
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
